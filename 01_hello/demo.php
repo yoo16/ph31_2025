509c617e0ee4b05b8f549b3c2a08bf8f0a645c1a
yoo16 <yohei.yoshikawa@gmail.com>
2025-04-24 15:46:01 +0900

    update

---
+++ 01_hello/demo.php
@@ -0,0 +1,18 @@
+<?php
+// 1. 変数は 先頭に $
+// 2. 行の最後は基本セミコロン ;
+$message = "きょうは曇りです";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>こんにちはー</h1>
+</body>
+</html>
