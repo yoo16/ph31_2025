50748b77ad0415a977c1e4ac6fb435d936f753ca
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-29 15:48:34 +0900

    update

--- 11_mysql/crud/select_users.php
+++ 11_mysql/crud/select_users.php
@@ -12,7 +12,7 @@ function get($limit = 50)
     // DB接続
     $pdo = Database::getInstance();
     // TODO: SQL作成：users テーブルから50件取得
-    $sql = "";
+    $sql = "SELECT * FROM users LIMIT {$limit}";
     // queryメソッドでSQLを query() で実行し、PDOStatementオブジェクトを取得
     $stmt = $pdo->query($sql);
     // TODO: Userデータを全件取得: fetchAll(PDO::FETCH_ASSOC)
