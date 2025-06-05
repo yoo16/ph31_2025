a4ff9ab7a7d46b8029b4c0cd505089d88f6e2338
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-05 16:52:06 +0900

    update

--- php_sns/user/logout/index.php
+++ php_sns/user/logout/index.php
@@ -3,6 +3,9 @@ require_once "../../app.php";
 
 if (isset($_SESSION[APP_KEY]['auth_user'])) {
     // TODO: セッションの APP_KEY 下の auth_user を削除: unset()
+    unset($_SESSION[APP_KEY]['auth_user']);
 }
 
 // TODO: ログイン画面にリダイレクト: ../../login/
+header('Location: ../../login/');
+exit;
