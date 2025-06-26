b9bd26725431d0bf351f68c227f861973a82d0ce
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-26 17:04:50 +0900

    update

--- php-sushi/api/category/fetch.php
+++ php-sushi/api/category/fetch.php
@@ -11,3 +11,9 @@ $categories = $categoryModel->fetch();
 // status: success
 // categories: $categories
 // オプション: JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
+$json = json_encode([
+    'status' => 'success',
+    'categories' => $categories
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+echo $json;
