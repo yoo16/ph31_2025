32f8d79454591f3e183c845494ca6730d98d1fba
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-09-18 16:03:55 +0900

    update

--- 18_sqlite/js/modal.js
+++ 18_sqlite/js/modal.js
@@ -1,9 +1,9 @@
 function openModal(imageUrl, title, body) {
     // TODO: 画像URLから画像を取得して表示
-    document.getElementById("modalImage").src;
+    document.getElementById("modalImage").src = imageUrl;
     // TODO: タイトルと本文を設定
-    document.getElementById("modalTitle").innerText;
-    document.getElementById("modalBody").innerHTML;
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalBody").innerHTML = body;
     // モーダルを表示
     document.getElementById("imageModal").classList.remove("hidden");
     // モーダルをフレックスボックスに設定して中央に配置
