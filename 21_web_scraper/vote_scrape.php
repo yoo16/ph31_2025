1b3813dae57e9e0863c0b7179880c79ca97503b8
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-02-04 16:44:41 +0900

    update

--- 21_web_scraper/vote_scrape.php
+++ 21_web_scraper/vote_scrape.php
@@ -34,36 +34,36 @@ try {
     // リクエストの送信
     $response = $client->request('GET', $url);
     // TODO: HTMLを取得
-    // $html = $response->getBody()->getContents();
+    $html = $response->getBody()->getContents();
 
     // TODO: HTMLをパース
-    // $crawler = new Crawler($html);
+    $crawler = new Crawler($html);
 
     // 候補者情報を抽出
     $data = $crawler->filter('.p_senkyoku_list_block')->each(function (Crawler $node) {
         // TODO: filter()で class="p_senkyoku_list_block_text_name .text" の要素を取得
-        // $nameNode = $node->filter('.p_senkyoku_list_block_text_name .text');
+        $nameNode = $node->filter('.p_senkyoku_list_block_text_name .text');
 
         // TODO: filter()で class="p_senkyoku_list_block_text_party" の要素を取得
-        // $partyNode = $node->filter('.p_senkyoku_list_block_text_party');
+        $partyNode = $node->filter('.p_senkyoku_list_block_text_party');
 
         // TODO: filter()で class="p_senkyoku_list_block_text_para span" の要素を取得
-        // $params = $node->filter('.p_senkyoku_list_block_text_para span')->each(function (Crawler $s) {
-        //     return trim($s->text());
-        // });
+        $params = $node->filter('.p_senkyoku_list_block_text_para span')->each(function (Crawler $s) {
+            return trim($s->text());
+        });
 
         // 候補者情報を抽出
-        // $name = trim(str_replace(' ', '', $nameNode->count() ? $nameNode->text() : '不明'));
-        // $party = trim($partyNode->count() ? $partyNode->text() : '無所属等');
-        // $age = $params[0] ?? '-';
-        // $job = $params[1] ?? '-';
+        $name = trim(str_replace(' ', '', $nameNode->count() ? $nameNode->text() : '不明'));
+        $party = trim($partyNode->count() ? $partyNode->text() : '無所属等');
+        $age = $params[0] ?? '-';
+        $job = $params[1] ?? '-';
 
         // 配列に追加
         return [
-            // 'name'  => $name,
-            // 'party' => $party,
-            // 'age'   => $age,
-            // 'job'   => $job,
+            'name'  => $name,
+            'party' => $party,
+            'age'   => $age,
+            'job'   => $job,
         ];
     });
 
@@ -77,10 +77,10 @@ try {
     $fileName = "{$dir}/{$senkyoku_id}.json";
 
     // TODO: 配列をJSONに変換: UNICODE対応、整形して保存
-    // $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
     // TODO: JSONファイルに書き込み
-    // file_put_contents($fileName, $json);
+    file_put_contents($fileName, $json);
 
     // 完了時に選挙区IDをクエリパラメータで渡す
     header("Location: vote.php?last_id={$senkyoku_id}");
